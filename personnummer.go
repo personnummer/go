158fe9a6d49a60278409a1b8b780b4ce1c6eceb5
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2019-10-03 14:29:53 +0200

    Add get age

--- personnummer.go
+++ personnummer.go
@@ -3,7 +3,10 @@ package personnummer
 import (
 	"errors"
 	"fmt"
+	"math"
 	"strconv"
+	"strings"
+	"time"
 )
 
 const (
@@ -11,15 +14,38 @@ const (
 	lengthWithCentury    = 12
 )
 
-func toString(in interface{}) string {
-	switch v := in.(type) {
-	case int, int32, int64, uint, uint32, uint64:
-		return fmt.Sprint(v)
-	case string:
-		return v
-	default:
-		return ""
+var (
+	invalidSecurityNumber = errors.New("Invalid swedish social security number")
+	monthDays             = map[int]int{
+		1:  31,
+		3:  31,
+		4:  30,
+		5:  31,
+		6:  30,
+		7:  31,
+		8:  31,
+		9:  30,
+		10: 31,
+		11: 30,
+		12: 31,
 	}
+	now   = time.Now
+	rule3 = [...]int{0, 2, 4, 6, 8, 1, 3, 5, 7, 9}
+)
+
+// charsToDigit converts char bytes to a digit
+// example: ['1', '1'] => 11
+func charsToDigit(chars []byte) int {
+	l := len(chars)
+	r := 0
+	for i, c := range chars {
+		p := int((c - '0'))
+		for j := 0; j < l-i-1; j++ {
+			p *= 10
+		}
+		r += p
+	}
+	return r
 }
 
 // getCleanNumber will return clean numbers.
@@ -47,18 +73,115 @@ func getCleanNumber(in string) []byte {
 	return cleanNumber
 }
 
-var monthDays = map[int]int{
-	1:  31,
-	3:  31,
-	4:  30,
-	5:  31,
-	6:  30,
-	7:  31,
-	8:  31,
-	9:  30,
-	10: 31,
-	11: 30,
-	12: 31,
+// getCoOrdinationDay will return co-ordination day.
+func getCoOrdinationDay(day []byte) []byte {
+	d := charsToDigit(day)
+	if d < 60 {
+		return day
+	}
+
+	d -= 60
+
+	if d < 10 {
+		return []byte{'0', byte(d) + '0'}
+	}
+
+	return []byte{
+		byte(d)/10 + '0',
+		byte(d)%10 + '0',
+	}
+}
+
+type dateParts struct {
+	LeapYear bool
+	FullYear int
+	Century  int
+	Year     int
+	Month    int
+	Date     int
+}
+
+func getDateParts(dateBytes []byte, plus bool) (*dateParts, error) {
+	switch len(dateBytes) {
+	case lengthWithCentury:
+		dateBytes = dateBytes[2:8]
+		break
+	case lengthWithoutCentury:
+		dateBytes = dateBytes[0:6]
+		break
+	}
+
+	length := len(dateBytes)
+	date := charsToDigit(dateBytes[length-2 : length])
+	month := charsToDigit(dateBytes[length-4 : length-2])
+
+	if month != 2 {
+		if _, ok := monthDays[month]; !ok {
+			return nil, invalidSecurityNumber
+		}
+	}
+
+	parts := &dateParts{
+		Date:  date,
+		Month: month,
+	}
+
+	if len(dateBytes[:length-4]) == 4 {
+		parts.FullYear = charsToDigit(dateBytes[:length-4])
+		parts.Century = charsToDigit(dateBytes[:length-4][0:2])
+		parts.Year = charsToDigit(dateBytes[:length-4][2:])
+	} else {
+		parts.Year = charsToDigit(dateBytes[:length-4])
+
+		var baseYear int
+		if plus {
+			baseYear = now().Year() - 100
+		} else {
+			baseYear = now().Year()
+		}
+
+		centuryStr := strconv.Itoa((baseYear - ((baseYear - parts.Year) % 100)))
+		century, err := strconv.Atoi(centuryStr[0:2])
+		if err != nil {
+			return nil, err
+		}
+
+		parts.Century = century
+		parts.FullYear = charsToDigit(append([]byte(centuryStr[0:2]), dateBytes[:length-4]...))
+	}
+
+	parts.LeapYear = parts.Year%4 == 0 && parts.Year%100 != 0 || parts.Year%400 == 0
+
+	return parts, nil
+}
+
+// luhn will test if the given string is a valid luhn string.
+func luhn(s []byte) bool {
+	odd := len(s) & 1
+
+	var sum int
+
+	for i, c := range s {
+		if i&1 == odd {
+			sum += rule3[c-'0']
+		} else {
+			sum += int(c - '0')
+		}
+	}
+
+	return sum%10 == 0
+}
+
+// toString converts int to string.
+func toString(in interface{}) string {
+	switch v := in.(type) {
+	case int, int32, int64, uint, uint32, uint64:
+		return fmt.Sprint(v)
+	case string:
+		return v
+	default:
+		return ""
+	}
 }
 
 // input time without centry.
@@ -132,57 +255,24 @@ func Valid(ssn interface{}, opts ...*Options) bool {
 	}
 }
 
-var rule3 = [...]int{0, 2, 4, 6, 8, 1, 3, 5, 7, 9}
-
-// luhn will test if the given string is a valid luhn string.
-func luhn(s []byte) bool {
-	odd := len(s) & 1
-
-	var sum int
-
-	for i, c := range s {
-		if i&1 == odd {
-			sum += rule3[c-'0']
-		} else {
-			sum += int(c - '0')
-		}
+// GetAge returns the age for a Swedish social security number.
+func GetAge(ssn interface{}, opts ...*Options) (int, error) {
+	if !Valid(ssn, opts...) {
+		return 0, invalidSecurityNumber
 	}
 
-	return sum%10 == 0
-}
+	str := toString(ssn)
+	cleanNumber := getCleanNumber(str)
 
-// getCoOrdinationDay will return co-ordination day.
-func getCoOrdinationDay(day []byte) []byte {
-	d := charsToDigit(day)
-	if d < 60 {
-		return day
+	parts, err := getDateParts(cleanNumber, strings.Contains(str, "+"))
+	if err != nil {
+		return 0, err
 	}
 
-	d -= 60
+	t := time.Date(parts.FullYear, time.Month(parts.Month), parts.Date, 0, 0, 0, 0, time.UTC)
+	age := math.Floor(now().Sub(t).Hours() / 24 / 365)
 
-	if d < 10 {
-		return []byte{'0', byte(d) + '0'}
-	}
-
-	return []byte{
-		byte(d)/10 + '0',
-		byte(d)%10 + '0',
-	}
-}
-
-// charsToDigit converts char bytes to a digit
-// example: ['1', '1'] => 11
-func charsToDigit(chars []byte) int {
-	l := len(chars)
-	r := 0
-	for i, c := range chars {
-		p := int((c - '0'))
-		for j := 0; j < l-i-1; j++ {
-			p *= 10
-		}
-		r += p
-	}
-	return r
+	return int(age), nil
 }
 
 // IsFemale checks if a Swedish social security number is for a female.
@@ -200,7 +290,7 @@ func IsFemale(ssn interface{}, opts ...*Options) (bool, error) {
 // The second argument should be a boolean
 func IsMale(ssn interface{}, opts ...*Options) (bool, error) {
 	if !Valid(ssn, opts...) {
-		return false, errors.New("Invalid swedish social security number")
+		return false, invalidSecurityNumber
 	}
 
 	cleanNumber := getCleanNumber(toString(ssn))
--- personnummer_test.go
+++ personnummer_test.go
@@ -2,6 +2,7 @@ package personnummer
 
 import (
 	"testing"
+	"time"
 
 	"github.com/frozzare/go-assert"
 )
@@ -75,48 +76,91 @@ func TestWrongCoOrdinationNumbers(t *testing.T) {
 	assert.False(t, Valid("640893-3231"))
 }
 
+func TestGetAge(t *testing.T) {
+	now = func() time.Time {
+		return time.Date(2019, 7, 13, 01, 01, 01, 01, time.UTC)
+	}
+
+	age, _ := GetAge(6403273813)
+	assert.Equal(t, 55, age)
+
+	age, _ = GetAge("510818-9167")
+	assert.Equal(t, 67, age)
+
+	age, _ = GetAge("19900101-0017")
+	assert.Equal(t, 29, age)
+
+	age, _ = GetAge("19130401+2931")
+	assert.Equal(t, 106, age)
+
+	age, _ = GetAge("200002296127")
+	assert.Equal(t, 19, age)
+}
+
+func TestGetAgeWithCoOrdinationNumbers(t *testing.T) {
+	now = func() time.Time {
+		return time.Date(2019, 7, 13, 01, 01, 01, 01, time.UTC)
+	}
+
+	age, _ := GetAge("701063-2391")
+	assert.Equal(t, 48, age)
+
+	age, _ = GetAge("640883-3231")
+	assert.Equal(t, 54, age)
+}
+
+func TestGetAgeExcludeCoOrdinationNumbers(t *testing.T) {
+	now = func() time.Time {
+		return time.Date(2019, 7, 13, 01, 01, 01, 01, time.UTC)
+	}
+
+	age, err := GetAge("701063-2391", &Options{
+		CoordinatioNumber: false,
+	})
+	assert.Equal(t, 0, age)
+	assert.NotNil(t, err)
+
+	age, err = GetAge("640883-3231", &Options{
+		CoordinatioNumber: false,
+	})
+	assert.Equal(t, 0, age)
+	assert.NotNil(t, err)
+}
+
 func TestSex(t *testing.T) {
-	valid, err := IsMale(6403273813, &Options{
+	valid, _ := IsMale(6403273813, &Options{
 		CoordinatioNumber: false,
 	})
 	assert.True(t, valid)
-	assert.Nil(t, err)
 
-	valid, err = IsFemale(6403273813, &Options{
+	valid, _ = IsFemale(6403273813, &Options{
 		CoordinatioNumber: false,
 	})
 	assert.False(t, valid)
-	assert.Nil(t, err)
 
-	valid, err = IsFemale("510818-9167", &Options{
+	valid, _ = IsFemale("510818-9167", &Options{
 		CoordinatioNumber: false,
 	})
 	assert.True(t, valid)
-	assert.Nil(t, err)
 
-	valid, err = IsMale("510818-9167", &Options{
+	valid, _ = IsMale("510818-9167", &Options{
 		CoordinatioNumber: false,
 	})
 	assert.False(t, valid)
-	assert.Nil(t, err)
 }
 
 func TestSexWithCoOrdinationNumbers(t *testing.T) {
-	valid, err := IsMale("701063-2391")
+	valid, _ := IsMale("701063-2391")
 	assert.True(t, valid)
-	assert.Nil(t, err)
 
-	valid, err = IsFemale("701063-2391")
+	valid, _ = IsFemale("701063-2391")
 	assert.False(t, valid)
-	assert.Nil(t, err)
 
-	valid, err = IsFemale("640883-3223")
+	valid, _ = IsFemale("640883-3223")
 	assert.True(t, valid)
-	assert.Nil(t, err)
 
-	valid, err = IsMale("640883-3223")
+	valid, _ = IsMale("640883-3223")
 	assert.False(t, valid)
-	assert.Nil(t, err)
 }
 
 func TestSexInvalidNumbers(t *testing.T) {
